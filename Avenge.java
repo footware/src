43a76f1793528c597af529c26fe5fb91aa815678
footware <footware@outlook.jp>
2018-05-05 22:40:43 +0900

    first commit

---
+++ Avenge.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class Avenge {
+
+	public static void main(String[] args) {
+		JFrame MainFrame = new JFrame("test");
+		MainFrame.setVisible(true);
+
+	}
+
+}
